47add9f38abcc5f2f545555d6178b1b2c05fe656
guisaodani <guisaodani@gmail.com>
2026-01-15 20:39:01 -0500

    complete exercise 1 on validating whether a number is pair or odd

---
+++ LogicPractice.Csharp/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿//NUMERO ES PAR O NO //
+
+var numberStr = string.Empty;
+do
+
+{
+    Console.Write("Ingrese un número entero o 'salir' para salir: ");
+    numberStr = Console.ReadLine();
+    if (numberStr!.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+
+    if (int.TryParse(numberStr, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es impar");
+        }
+    }
+    else { Console.WriteLine($"{numberStr} no es un numero valido."); }
+} while (numberStr!.ToLower() != "salir");
+Console.WriteLine("Fin");
