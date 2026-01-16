1dcd9e4b744dcdad9686540a56658fac8b286468
guisaodani <guisaodani@gmail.com>
2026-01-15 21:19:59 -0500

    complete exercise 2 on three numbers validating whether a numero is major others numbers

---
+++ LogicPractice.Csharp/TheGreatestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿//Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor de los números ingresados.
+
+int a, b, c;
+bool valido;
+
+do
+{
+    Console.Write("INGRESE TRES NUMEROS. \n");
+
+    Console.Write("Ingrese primer numero: ");
+    valido = int.TryParse(Console.ReadLine(), out a);
+
+    Console.Write("Ingrese segundo numero: ");
+    valido &= int.TryParse(Console.ReadLine(), out b);
+
+    Console.Write("Ingrese tercer numero: ");
+    valido &= int.TryParse(Console.ReadLine(), out c);
+
+    if (a == b || a == c || b == c)
+    {
+        Console.WriteLine("Los números deben ser diferentes.\n");
+        continue;
+    }
+
+    if (a > b && a > c)
+        Console.WriteLine($"El mayor es: {a}");
+    else if (b > a && b > c)
+        Console.WriteLine($"El mayor es: {b}");
+    else
+        Console.WriteLine($"El mayor es: {c}");
+    break;
+} while (true);
