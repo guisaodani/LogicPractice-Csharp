a348c2dd3034da5126152dc0ecc15ba8dfdf5508
guisaodani <guisaodani@gmail.com>
2026-01-19 21:10:03 -0500

    algorithm created is multiple number anothre

---
+++ LogicPractice.Csharp/IsMultipleAnother/Program.cs
@@ -0,0 +1,16 @@
+﻿//Construir un programa que pida dos número y diga si el segundo es múltiplo del primero.
+using Shared;
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primero numero: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo numero ");
+
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{b} es multiplo de {a}");
+    }
+    else
+        Console.WriteLine($"{b} no es multiplo de {a}");
+}
+while (true);
