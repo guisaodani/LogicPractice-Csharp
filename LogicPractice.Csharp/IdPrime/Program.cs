39a64d9999c64969b85382d29f3e0a88d0aab97f
guisaodani <guisaodani@gmail.com>
2026-01-22 23:00:44 -0500

    is prime

---
+++ LogicPractice.Csharp/IdPrime/Program.cs
@@ -0,0 +1,29 @@
+﻿using Shared;
+using System.Security.Cryptography;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingresa Numero: ");
+    var isprime = MyMath.IsPrime(n);
+    Console.WriteLine($"El numero: {n} {(isprime ? "SI" : "NO")} es primo");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar S[si], N[no] ?: ", options);
+        Console.WriteLine();
+    }
+    while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+}
+while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+//bool IsPrime(int n)
+//{
+//    for (int i = 2; i <= Math.Sqrt(n); i++)
+//    {
+//        if (n % i == 0) return false;
+//    }
+//    return true;
+//}
--- LogicPractice.Csharp/Shared/MyMath.cs
+++ LogicPractice.Csharp/Shared/MyMath.cs
@@ -19,4 +19,14 @@ namespace Shared
             return factorial;
         }
     }
+
+    public static bool IsPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
 }
